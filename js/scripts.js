a492a42623fff50c418c1892d1b9cf1a29f47732
Matthew Giem and Patrick Lipscomb <matthew@>
2016-06-09 13:56:17 -0700

    initialize the templet for the page

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var romanNumeralsArray = [];
+$(document).ready(function() {
+  $('form#input').submit(function(event) {
+    event.preventDefault();
+    var userInput = $('input#number');
+
+    $('#result').text(answer);
+  });
+});
+
+var cryptoSquare = function(sentence) {
+  var string = sentence.replace(/\s+/g, '');
+
+}
