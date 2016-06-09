86617cbd91bc18acf92684a52cbe5bff80bb21ed
Matthew Giem and Patrick Lipscomb <matthew@>
2016-06-09 14:17:24 -0700

    create the function to make the code from any string

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,21 @@ var romanNumeralsArray = [];
 $(document).ready(function() {
   $('form#input').submit(function(event) {
     event.preventDefault();
-    var userInput = $('input#number');
-
-    $('#result').text(answer);
+    var userInput = $('input#number').val();
+    cryptoSquare(userInput);
+    // $('#result').text(answer);
   });
 });
 
 var cryptoSquare = function(sentence) {
-  var string = sentence.replace(/\s+/g, '');
-
+var string = sentence.replace(/\s/g,'');
+var codeWord='';
+debugger;
+var boxCol = Math.floor(Math.sqrt(string.length));
+  for (var j = 0; j < boxCol; j++) {
+    for (var i = j; i < string.length; i += boxCol) {
+      codeWord +=(string.charAt(i));
+    }
+  }
+  alert(codeWord);
 }
