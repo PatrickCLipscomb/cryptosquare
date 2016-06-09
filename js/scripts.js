ed217c16988c558aa3fc4f954522b3c14af8bbc0
Matthew Giem and Patrick Lipscomb <patrick@>
2016-06-09 14:36:13 -0700

    create a function that puts a space ever 5 char

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,22 @@ $(document).ready(function() {
   });
 });
 
+var insertAtt = function(string, index , subString) {
+  return string.substr(0, index) + subString + string.substr(index);
+}
+
+
 var cryptoSquare = function(sentence) {
 var string = sentence.replace(/\s/g,'');
 var codeWord='';
-debugger;
 var boxCol = Math.floor(Math.sqrt(string.length));
   for (var j = 0; j < boxCol; j++) {
     for (var i = j; i < string.length; i += boxCol) {
       codeWord +=(string.charAt(i));
     }
   }
+  debugger;
+  codeWord = insertAtt(codeWord, 5, " ");
+
   alert(codeWord);
 }
